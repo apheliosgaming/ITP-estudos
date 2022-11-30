595d12f75c43f461bbe77bb2b369f9ba9bd9092a
ian <108193413+apheliosgaming@users.noreply.github.com>
2022-11-30 17:13:31 -0300

    Add files via upload

---
+++ listas/sobre ponteiros.c
@@ -0,0 +1,7 @@
+// nenhum codigo so anotação de ponteiros
+
+// quando a função precisa retornar mais de uma valor se utiliza ponteiros
+// outro cenario eh quando se trabalha c uma quantidade de dados mt grande p ser alocada na stack de memoria do computador
+// outro cenario eh quando dentro da funçao vc precisa retornar uma estrutura que vc criou na funçao
+// tb quando quero criar estruturas que variam de tamanho dinamicamente na memoria (ponteiros e alocaçao dinamica)
+// '&' para passar o endereço e nao valor
